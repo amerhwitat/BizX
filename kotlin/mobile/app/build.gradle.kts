553130757ddf916e1ec80eab6201dca706f6ce6b
Amer Hwitat <32159380+amerhwitat@users.noreply.github.com>
2026-09-12 15:54:55 +0300

    Add BizX Kotlin Android module

---
+++ kotlin/mobile/app/build.gradle.kts
@@ -0,0 +1,19 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.amerhwitat.bizx.mobile"
+    compileSdk = 36
+    defaultConfig {
+        applicationId = "com.amerhwitat.bizx.mobile"
+        minSdk = 26
+        targetSdk = 36
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+    buildTypes { release { isMinifyEnabled = false } }
+}
+
+kotlin { jvmToolchain(17) }
