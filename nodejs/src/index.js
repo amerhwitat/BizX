c8c9319c0c47a15e4b8eaa21960e6713cd8c18c6
Amer Hwitat <32159380+amerhwitat@users.noreply.github.com>
2026-09-13 15:11:27 +0300

    feat(nodejs): expose unified runtime through public API

--- nodejs/src/index.js
+++ nodejs/src/index.js
@@ -1,6 +1,7 @@
 import { BizXCore } from './core/index.js';
 export { BizXCore } from './core/index.js';
 export { WalletProvider, createWalletProvider } from './wallet/index.js';
+export { UnifiedBizXRuntime, createUnifiedBizX, FEATURE_FAMILIES, Vector3, PerspectiveCamera, AssetCatalog, GameStore, NetworkUnified, InternetScanner, AssetBrowser, CryptoService, WebService, RenderingService, ScriptService } from './unified/index.js';
 
 export function createBizX(options = {}) {
   return new BizXCore(options);
